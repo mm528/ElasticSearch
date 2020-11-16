2d814bea334bb9e8454c78d7e1ebd1c12f6efa1f
Michalis Motis <motisano@gmail.com>
2020-11-16 14:10:36 +0200

    commit

--- guiStart.py
+++ guiStart.py
@@ -1,59 +1,59 @@
-# from __future__ import print_function
-# from collections import UserString
-# from elasticsearch.exceptions import NotFoundError
-# import sys
+from __future__ import print_function
+from collections import UserString
+from elasticsearch.exceptions import NotFoundError
+import sys
 
-# from PyQt5.QtWidgets import QLabel,QMainWindow,QApplication, QWidget, QPushButton,QTextEdit
-# from PyQt5.QtGui import QIcon
-# from PyQt5 import uic
-# from PyQt5.QtCore import pyqtSlot
-# from elasticsearch import Elasticsearch
-# from elasticsearch_dsl import search
-# es = Elasticsearch("http://localhost:9200")
-# import json 
-# import os
-# from elasticsearch_dsl import Search
-# from datetime import datetime
-# from elasticsearch_dsl import Document, Date, Integer, Keyword, Text
-# from elasticsearch_dsl.connections import connections
-
-
-
-# import argparse
-# from elasticsearch_dsl.query import Q
+from PyQt5.QtWidgets import QLabel,QMainWindow,QApplication, QWidget, QPushButton,QTextEdit
+from PyQt5.QtGui import QIcon
+from PyQt5 import uic
+from PyQt5.QtCore import pyqtSlot
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import search
+es = Elasticsearch("http://localhost:9200")
+import json 
+import os
+from elasticsearch_dsl import Search
+from datetime import datetime
+from elasticsearch_dsl import Document, Date, Integer, Keyword, Text
+from elasticsearch_dsl.connections import connections
 
 
 
-# class UI (QMainWindow):
-#     def __init__(self):
-#         super(UI,self).__init__()
+import argparse
+from elasticsearch_dsl.query import Q
 
-#         uic.loadUi("C:/Users/motis/Desktop/groupPython/guiTest.ui",self)
-#         self.label = self.findChild(QLabel,"label")
-#         self.button = self.findChild(QPushButton, "pushButton")
-#         self.button2 = self.findChild(QPushButton, "pushButton_2")
-#         self.text = self.findChild(QTextEdit,"textEditRight")
-#         self.text = self.findChild(QTextEdit,"textEditLeft")
 
-#         button = QPushButton('Hey', self)
-#         button.setToolTip('This is an example button')
-#         button.move(100,70)
+
+class UI (QMainWindow):
+    def __init__(self):
+        super(UI,self).__init__()
+
+        uic.loadUi("C:/Users/motis/Desktop/groupPython/guiTest.ui",self)
+        self.label = self.findChild(QLabel,"label")
+        self.button = self.findChild(QPushButton, "pushButton")
+        self.button2 = self.findChild(QPushButton, "pushButton_2")
+        self.text = self.findChild(QTextEdit,"textEditRight")
+        self.text = self.findChild(QTextEdit,"textEditLeft")
+
+        button = QPushButton('Hey', self)
+        button.setToolTip('This is an example button')
+        button.move(100,70)
         
 
 
-#         button.clicked.connect(self.on_click)
-#         self.button.clicked.connect(self.clk)
-#         self.button2.clicked.connect(self.click2)
-#         self.show()
+        button.clicked.connect(self.on_click)
+        self.button.clicked.connect(self.clk)
+        self.button2.clicked.connect(self.click2)
+        self.show()
 
-#     @pyqtSlot()
-#     def on_click(self):
-#          self.label.setText('Connect with Elastic SEARCH! Print results') 
-#          print('Connect with Elastic SEARCH! Print results')
-#          print('Test git')
+    @pyqtSlot()
+    def on_click(self):
+         self.label.setText('Connect with Elastic SEARCH! Print results') 
+         print('Connect with Elastic SEARCH! Print results')
+         print('Test git')
 
-#     def click2(self):
-#           print('Here i need to find a connection between the words')
+    def click2(self):
+          print('Here i need to find a connection between the words')
           
 
 
@@ -61,57 +61,57 @@
 
 
 
-#     def clk(self):
+    def clk(self):
           
-#           #retrieve data for id=2
+          #retrieve data for id=2
           
-#           res = es.get(index="news", id='tLxuy3UBg-YlbYnR9IRP')
-#           j =  'Trying to find with the exact word                [all]   ' + json.dumps(res['_index'])
-#           k = json.dumps(res['_source']['text'])
-#           print (res)
-#           self.label.setText(j)
-#           self.text.setText(str(k))  
-#           for hit in res:
-#             print(hit)
+          res = es.get(index="news", id='tLxuy3UBg-YlbYnR9IRP')
+          j =  'Trying to find with the exact word                [all]   ' + json.dumps(res['_index'])
+          k = json.dumps(res['_source']['text'])
+          print (res)
+          self.label.setText(j)
+          self.text.setText(str(k))  
+          for hit in res:
+            print(hit)
 
-#           text = "London"
-#           index = "cities"
-#           try:
-#              client = Elasticsearch()
-#              s = Search(using=client, index="cities")
-#              if text is not None:
-#                     q = Q('multi_match', query=text, fields=['text'])
-#                     s = s.query(q)
-#                     s = s.highlight('text', fragment_size=10)
-#                     response = s.execute()
-#                     for r in s.scan(): # scan allows to retrieve all matches
-#                      print('ID= %s PATH=%s' % (r.meta.id, r.path))
-#                      for j, fragment in enumerate(r.meta.highlight.text):
-#                         print(' ->  TXT=%s' % fragment) 
+          text = "London"
+          index = "cities"
+          try:
+             client = Elasticsearch()
+             s = Search(using=client, index="cities")
+             if text is not None:
+                    q = Q('multi_match', query=text, fields=['text'])
+                    s = s.query(q)
+                    s = s.highlight('text', fragment_size=10)
+                    response = s.execute()
+                    for r in s.scan(): # scan allows to retrieve all matches
+                     print('ID= %s PATH=%s' % (r.meta.id, r.path))
+                     for j, fragment in enumerate(r.meta.highlight.text):
+                        print(' ->  TXT=%s' % fragment) 
            
-#                     q = Q('query_string',query='London')
-#                     s = s.query(q)
-#                     response = s.execute()
-#                     for r in s.scan(): # scan allows to retrieve all matches
-#                      print('ID= %s TXT=%s PATH=%s' % (r.meta.id, r.text[0:20], r.path))
-#                      print ('%d Documents' % response.hits.total.value) 
-#           except NotFoundError:
-#             print('Index %s does not exists' % index)   
+                    q = Q('query_string',query='London')
+                    s = s.query(q)
+                    response = s.execute()
+                    for r in s.scan(): # scan allows to retrieve all matches
+                     print('ID= %s TXT=%s PATH=%s' % (r.meta.id, r.text[0:20], r.path))
+                     print ('%d Documents' % response.hits.total.value) 
+          except NotFoundError:
+            print('Index %s does not exists' % index)   
          
            
            
 
 
-#           #s = Search(using=client, index="cities") \
-#            #.query("match", _source="London")
+          #s = Search(using=client, index="cities") \
+           #.query("match", _source="London")
            
 
-#           #os.system('C:/Users/motis/Desktop/groupPython/SearchIndex.py')            
+          #os.system('C:/Users/motis/Desktop/groupPython/SearchIndex.py')            
     
 
 
 
 
-# app = QApplication(sys.argv)
-# UIWindow = UI()
-# app.exec_()
+app = QApplication(sys.argv)
+UIWindow = UI()
+app.exec_()
