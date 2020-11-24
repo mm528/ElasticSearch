e09a52df240745ef52996cfe35bdd339f0a4ff4d
Michalis Motis <motisano@gmail.com>
2020-11-24 22:17:22 +0200

    commit

--- csvtojson.py
+++ csvtojson.py
@@ -1,14 +1,14 @@
 import csv
 import json
 
-file = open("netflix_with_rating2.csv", "r")
+file = open("netflix_with_rating.csv", "r", encoding='UTF8') #Edw to diavazi me UTF8 (SIMANTIKO)
 dict_reader = csv.DictReader(file)
 
 
 dict_from_csv = list(dict_reader)
 json_from_csv = json.dumps(dict_from_csv)
-with open("netflix_with_rating.json2", "w") as outfile: 
-    
+with open("michalis.json", "w") as outfile: 
+    # outfile.write('[')
     outfile.write(json_from_csv)
-    
+    # outfile.write(']')
 print(json_from_csv)
