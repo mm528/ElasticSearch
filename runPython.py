fd0c8dc4f10050c95d24d7a190bfb0661bea64c6
Michalis Motis <motisano@gmail.com>
2020-11-27 14:47:12 +0200

    push

--- runPython.py
+++ runPython.py
@@ -19,7 +19,7 @@ with open ("C:/Users/motis/Desktop/finallyProject/ElasticSearch/password.txt", '
 #print(nameIndex + " " + typeName + " " +path )
 script_descriptor = open("elasticsearch_loader.py")
 a_script = script_descriptor.read()
-sys.argv = ["-i elasticsearch_loader.py", "--index", "movies", "--type","movies", "json" , "movies.json"] 
+sys.argv = ["-i elasticsearch_loader.py", "--index", "imdb", "--type", "imdb", "json" , "IMDB.json"] 
 #sys.argv = ["-i elasticsearch_loader.py", "--index", x[0], "--type", x[1], "json" , x[2]] >>>>>> storing into a txt file and passing over here the data
 #but it wasnt able fully functional 
 
